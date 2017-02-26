9973814df3e147d0b1389957491c5d1b92f6a0eb
jhonny10 <201325583@ingenieria.usac.edu.gt>
2017-02-25 18:37:30 -0600

    v1

---
+++ [EDD]Practica2_201325583/Nodo.py
@@ -0,0 +1,16 @@
+class Nodo:
+	sig = Nodo()
+	ant = Nodo()
+
+	def __init__(self):
+		self.dato = ""
+
+	def getSize(self)
+		return self.size
+
+	def getSig(self)
+		return self.sig
+
+	def getAnt(self)
+		return self.ant
+	
